ebcbe471c1bacdb62d1e9f320a2e8e8624f66da3
Graeme Lockley <graemel@no9.co.za>
2017-06-25 16:08:48 +0200

    refactor: libraryPath and fullLibraryPath

--- test/MRequireTest.js
+++ test/MRequireTest.js
@@ -6,3 +6,9 @@ const mrequire = require("../lib/mrequire");
 Assert.equal(mrequire.dirExists("./test"), true);
 Assert.equal(mrequire.dirExists("./test/MRequireTest.js"), false);
 Assert.equal(mrequire.dirExists("./bob"), false);
+
+
+Assert.equal(mrequire.libraryPath(["core", "Data.Int", "1.2.3"]), process.env.HOME + "/.sle/core/Data.Int");
+
+
+Assert.equal(mrequire.fullLibraryPath(["core", "Data.Int", "1.2.3"]), process.env.HOME + "/.sle/core/Data.Int/1.2.3");
