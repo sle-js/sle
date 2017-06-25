7fd4dd5784c3bd59135076cfc9f0b4ff4e4b78fd
Graeme Lockley <graemel@no9.co.za>
2017-06-25 15:46:08 +0200

    test: add further tests to confirm dirExists

--- test/MRequireTest.js
+++ test/MRequireTest.js
@@ -3,5 +3,6 @@ const Assert = require("assert");
 const mrequire = require("../lib/mrequire");
 
 
-Assert.equal(mrequire.dirExists("."), true);
+Assert.equal(mrequire.dirExists("./test"), true);
+Assert.equal(mrequire.dirExists("./test/MRequireTest.js"), false);
 Assert.equal(mrequire.dirExists("./bob"), false);
