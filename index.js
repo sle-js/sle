9167eade206b1e221258eedf78bccda7b8c811e1
Graeme Lockley <graemel@no9.co.za>
2017-06-30 21:56:08 +0200

    feature: add support for assumptions by attaching their functions at a global level

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 global.mrequire = require("./lib/mrequire").mrequire;
 
+const Assumption = mrequire("core:System.Assumption:1.0.0");
+
+
+global.assumption = Assumption.assumption;
+global.assumptionEqual = Assumption.assumptionEqual;
 
 module.exports = {};
 
