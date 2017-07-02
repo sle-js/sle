f36c4131c42a66ca984b138fe7cd856557ddba61
Graeme Lockley <graemel@no9.co.za>
2017-07-02 09:06:04 +0200

    fix: change the script not to rely on realpath which is available on macs but not linux

--- bin/sle.js
+++ bin/sle.js
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-REAL_PATH_NAME=`realpath $0`
+_realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+ }
+
+REAL_PATH_NAME=`_realpath $0`
 SCRIPT_HOME=`dirname $REAL_PATH_NAME`
 
 node --require ${SCRIPT_HOME}/../index.js $*
